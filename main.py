5eba0a553335752dd3074bf986747e9b6b900f30
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-06-10 18:20:03 +0000

    add torch

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 app = FastAPI()
@@ -14,14 +15,14 @@ tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("credentials.html", {"request": request})
+def home():
+    return templates.TemplateResponse("credentials.html")
 
 @app.post("/chatbot")
 async def chatbot(request: Request):
     data = await request.json()
     question = data.get("question")
-
+    
     inputs = tokenizer.encode(question, return_tensors="pt")
     outputs = model.generate(inputs, max_length=100, num_return_sequences=1)
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
