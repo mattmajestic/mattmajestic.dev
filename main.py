2fe1228bfb4796c1eedacf09eea0cc0d3cfd22d2
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-06-14 23:11:00 +0000

    retro css update

--- main.py
+++ main.py
@@ -10,35 +10,36 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-model = GPT2LMHeadModel.from_pretrained("gpt2")
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+@app.get("/credentials")
+def credentials(request: Request):
     return templates.TemplateResponse("credentials.html", {"request": request})
 
 @app.post("/chatbot")
 async def chatbot(request: Request):
     data = await request.json()
     question = data.get("question")
-    
+    model = GPT2LMHeadModel.from_pretrained("gpt2")
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
     inputs = tokenizer.encode(question, return_tensors="pt")
     outputs = model.generate(inputs, max_length=100, num_return_sequences=1)
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-
     return {"response": response}
 
 @app.get("/conversation")
-def conversation(request: Request):
+async def conversation(request: Request):
     return templates.TemplateResponse("conversation.html", {"request": request})
 
 @app.get("/download-resume-pdf")
-def download_resume():
+async def download_resume():
     resume_file_path = "static/resume.pdf"
     return FileResponse(resume_file_path, filename="resume.pdf")
     
 @app.get("/donate")
-def donate(request: Request):
+async def donate(request: Request):
     return templates.TemplateResponse("stripe.html", {"request": request})
